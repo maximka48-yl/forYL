6280d7f906e63389c9930dded02a1333b51324f6
Maximka48-YL <57672645+Maximka48-YL@users.noreply.github.com>
2020-03-24 16:57:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,15 @@
-import flask
+import os
 
-app = flask.Flask(__name__)
+from flask import Flask
 
-
-@app.route('/')
-def a():
-    return """<!doctype html>
-                    <html lang="en">
-                      <body>
-                        Миссия Колонизация Марса
-                      </body>
-                    </html>"""
+app = Flask(__name__)
 
 
-@app.route('/index')
+@app.route("/")
 def index():
-    return """<!doctype html>
-                <html lang="en">
-                  <body>
-                    И на Марсе будут яблони цвести!
-                  </body>
-                </html>"""
-
-
-@app.route('/promotion')
-def promotion():
-    return """<!doctype html>
-                <html lang="en">
-                  <body>
-                    Человечество вырастает из детства.</br>
-                    Человечеству мала одна планета.</br>
-                    Мы сделаем обитаемыми безжизненные пока планеты.</br>
-                    И начнем с Марса!</br>
-                    Присоединяйся!
-                  </body>
-                </html>"""
-
-
-@app.route('/image_mars')
-def image_mars():
-    return """<!doctype html>
-                <html lang="en">
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="/img/mars.jpg"></br>
-                    Вот она какая, красная планета.
-                  </body>
-                </html>"""
+    return "Привет от приложения Flask"
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
