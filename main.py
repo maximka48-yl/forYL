9dc041d9a809f6ce50d9528e2cdfddaf6829b121
Maximka48-YL <57672645+Maximka48-YL@users.noreply.github.com>
2020-03-24 16:34:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,52 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
+def a():
+    return """<!doctype html>
+                    <html lang="en">
+                      <body>
+                        Миссия Колонизация Марса
+                      </body>
+                    </html>"""
+
+
+@app.route('/index')
 def index():
-    return "Привет от приложения Flask"
+    return """<!doctype html>
+                <html lang="en">
+                  <body>
+                    И на Марсе будут яблони цвести!
+                  </body>
+                </html>"""
+
+
+@app.route('/promotion')
+def promotion():
+    return """<!doctype html>
+                <html lang="en">
+                  <body>
+                    Человечество вырастает из детства.</br>
+                    Человечеству мала одна планета.</br>
+                    Мы сделаем обитаемыми безжизненные пока планеты.</br>
+                    И начнем с Марса!</br>
+                    Присоединяйся!
+                  </body>
+                </html>"""
+
+
+@app.route('/image_mars')
+def image_mars():
+    return """<!doctype html>
+                <html lang="en">
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="/img/mars.jpg"></br>
+                    Вот она какая, красная планета.
+                  </body>
+                </html>"""
 
 
 if __name__ == '__main__':
